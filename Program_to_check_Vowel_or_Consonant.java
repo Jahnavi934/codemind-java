bb11d253630ce9e348def1e8402cee08cff66ff7
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2022-05-27 19:47:14 +0530

    import java.util.Scanner; class vowel { 	public static void main(String[] args) 	{ 		char ch; 		Scanner sc=new Scanner(System.in); 		ch=sc.next().charAt(0); 		if(ch=='A' || ch=='E' ||ch=='I' ||ch=='O' ||ch=='U'||ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u') 		{ 			System.out.println("Vowel"); 		} 		else 		{ 			System.out.println("Consonant"); 		} 	} }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class vowel
+{
+	public static void main(String[] args)
+	{
+		char ch;
+		Scanner sc=new Scanner(System.in);
+		ch=sc.next().charAt(0);
+		if(ch=='A' || ch=='E' ||ch=='I' ||ch=='O' ||ch=='U'||ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u')
+		{
+			System.out.println("Vowel");
+		}
+		else
+		{
+			System.out.println("Consonant");
+		}
+	}
+}
