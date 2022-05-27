7ad2d29a8c3acbbf72c0c7c1aefcf40994164313
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2022-05-27 20:21:09 +0530

    import java.util.Scanner; class a { 	public static void main(String args[]) 	{ 		Scanner sc= new Scanner(System.in); 		int i,j,n; 		n=sc.nextInt(); 		for(i=n;i>0;i--) 		{ 		    for(j=1;j<=i;j++) 		    { 		        System.out.print(j); 		    } 		    System.out.println(); 		} 	} }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class a
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int i,j,n;
+		n=sc.nextInt();
+		for(i=n;i>0;i--)
+		{
+		    for(j=1;j<=i;j++)
+		    {
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+	}
+}
